20f78cc64953e284ea15640d3ab46200e521a4ab
Bianca Aspin <bianca@mj94.net>
2023-01-10 19:33:15 -0800

    Recreated functions to review lab in Phase 1

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Write your solution here!
+
+// Test #1
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Test #2
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+// Test #3
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+// Test #4
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+// Test #5
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+// Test #6
+
+function appendCat(name) {
+    const newCats = [...cats, name];
+    return newCats;
+}
+
+// Test #7
+
+function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats;
+}
+
+// Test #8
+
+function removeLastCat() {
+    const newCats = [...cats.slice(0, -1)];
+    return newCats;
+}
+
+// Test #9
+
+function removeFirstCat() {
+    const newCats = [...cats.slice(1)];
+    return newCats;
+}
